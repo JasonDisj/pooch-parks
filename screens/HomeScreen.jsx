90da7b5ae56b21c2260a23729b9cbc89f71fe375
Jason Di <jasondisj@gmail.com>
2023-01-27 15:30:00 -0800

    initial commit

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
