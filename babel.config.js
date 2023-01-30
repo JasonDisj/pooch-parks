36f2adececf6e5f642a5a5f002f0c16346c24241
Jason Di <jasondisj@gmail.com>
2023-01-30 11:49:34 -0800

    fix bug by resetting valid plugin property

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      ["module:react-native-dotenv"],
-      { moduleName: "@env", path: ".env" },
+      ["module:react-native-dotenv", { moduleName: "@env", path: ".env" }],
     ],
   };
 };
