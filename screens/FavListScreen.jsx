f87e45d05e01bedaba003e52d58db5ff97ae0a87
Jason Di <jasondisj@gmail.com>
2023-02-05 12:12:21 -0800

    initial commit

---
+++ screens/FavListScreen.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const FavListScreen = () => {
+  return (
+    <View>
+      <Text>FavListScreen</Text>
+    </View>
+  );
+};
+
+export default FavListScreen;
+
+const styles = StyleSheet.create({});
